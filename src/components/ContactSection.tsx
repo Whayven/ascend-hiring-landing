b988f9c23f0226688e98906c127da4d9f9209208
Wayne <waynefosterjr@hotmail.com>
2023-10-17 16:20:21 -0400

    downgraded next to fix runtime error

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -152,6 +152,7 @@ export default function ContactForm() {
                 rows={4}
                 className='focus:ring-primary-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
                 defaultValue=''
+                placeholder='Feel free to leave a suggestion or send us a message.'
               />
             </div>
           </div>
