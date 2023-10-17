3eab4570e8b9e6c79b9a6f481c8e807d4db4a913
Wayne <waynefosterjr@hotmail.com>
2023-10-17 18:51:46 -0400

    added toasts, text changes in contact, reset form

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -3,6 +3,7 @@ import { Switch } from '@headlessui/react';
 import Link from 'next/link';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 interface IFormInput {
   firstName: string;
@@ -19,7 +20,12 @@ function classNames(...classes: string[]) {
 export default function ContactForm() {
   const [agreed, setAgreed] = useState(false);
 
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<IFormInput>();
 
   const onSubmit: SubmitHandler<IFormInput> = async (data: IFormInput) => {
     const request = {
@@ -31,13 +37,12 @@ export default function ContactForm() {
     };
 
     try {
-      const response = await fetch('/api/email', request);
-
-      if (response.ok) {
-        console.log('Email sent successfully');
-      } else {
-        console.error('Failed to send the email');
-      }
+      toast.promise(fetch('/api/email', request), {
+        loading: 'Sending...',
+        success: 'Email sent!',
+        error: 'Failed to send email',
+      });
+      reset();
     } catch (error) {
       console.error('Error:', error);
     }
@@ -85,6 +90,9 @@ export default function ContactForm() {
                 autoComplete='given-name'
                 className='focus:ring-primary-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
               />
+              {errors.firstName && (
+                <span className='text-red-500'>This field is required</span>
+              )}
             </div>
           </div>
           <div>
@@ -102,6 +110,9 @@ export default function ContactForm() {
                 autoComplete='family-name'
                 className='focus:ring-primary-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
               />
+              {errors.lastName && (
+                <span className='text-red-500'>This field is required</span>
+              )}
             </div>
           </div>
           <div className='sm:col-span-2'>
@@ -119,6 +130,9 @@ export default function ContactForm() {
                 autoComplete='organization'
                 className='focus:ring-primary-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
               />
+              {errors.company && (
+                <span className='text-red-500'>This field is required</span>
+              )}
             </div>
           </div>
           <div className='sm:col-span-2'>
@@ -136,6 +150,9 @@ export default function ContactForm() {
                 autoComplete='email'
                 className='focus:ring-primary-600 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6'
               />
+              {errors.email && (
+                <span className='text-red-500'>This field is required</span>
+              )}
             </div>
           </div>
 
@@ -194,7 +211,7 @@ export default function ContactForm() {
             type='submit'
             className='bg-primary-600 hover:bg-primary-500 focus-visible:outline-primary-600 block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
           >
-            Let's talk
+            Join the waitlist
           </button>
         </div>
       </form>
