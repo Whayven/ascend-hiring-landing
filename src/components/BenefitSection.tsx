18ab8552b0f727f8e7a34267391256e5c985ac36
Wayne <waynefosterjr@hotmail.com>
2023-10-17 19:21:38 -0400

    changed benefits section text

--- src/components/BenefitSection.tsx
+++ src/components/BenefitSection.tsx
@@ -38,7 +38,7 @@ export default function BenefitSection() {
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
           <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-            Stay on top of customer support
+            Transform your hiring process and unlock success.
           </h2>
           <dl className='col-span-2 grid grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2'>
             {benefits.map((benefit) => (
