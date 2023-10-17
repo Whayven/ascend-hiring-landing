8be142316596d729b631d8947a926a059bdd9ee7
Wayne <waynefosterjr@hotmail.com>
2023-10-17 14:01:01 -0400

    og update

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -3,5 +3,5 @@ export const siteConfig = {
   description:
     'An AI powered ATS that helps you find the best candidates for your company.',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://ascendhiring.com',
 };
