3001c7b60d626abc73413873980167073f71261c
DeDeveloper23 <abhishekde.ny@gmail.com>
2024-02-23 13:37:25 -0500

    feat: reduce title tag length

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: 'Ascend Hiring',
   title:
-    'Best ATS for Rapid Candidate Matching With Automated Interview Scheduling',
+    'Best ATS for Rapid Candidate Matching & Automated Interview Scheduling',
   description:
     'A Next Generation ATS that Connects you with the Best Candidates for your Company.',
   url: 'https://ascendhiring.com',
